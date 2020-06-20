f6ce3381956ee86bacd8b57c71091def04d48289
Will Saymon <wsaymonoficial@gmail.com>
2020-06-20 15:54:42 -0300

    Add babel to build

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
-import express from 'express';
-import { UserController } from '@controllers/UserController';
+import express from 'express'
+import { UserController } from '@controllers/UserController'
 
-const app = express();
-
+const app = express()
 
 app.get('/', (request, response) => {
-    return response.json({
-        message: 'Hello World!'
-    });
-});
+  return response.json({
+    message: 'Hello World!'
+  })
+})
 
-
-app.listen(3333).on('connection', () => {
-    console.log('listen on port 3333');
-});
+app.listen(3333).on('listening', () => {
+  console.log('listening on port 3333')
+})
